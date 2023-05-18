bbbf445f1d76ce45a7fcc1f72a46a667533675b6
Angela Palaszewski <delarosa.ang@gmail.com>
2023-05-18 12:47:07 -0600

    added art

--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+import sys
 from functions import *
 from time import sleep
-import sys
+from art import *
 from models import User, Result
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -11,8 +12,9 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 # Add music and art
-
-print ("Welcome to Hogwarts!!")
+print(castle)
+sleep(.5)
+print (welcome)
 sleep(1)
 in_game = False
 while True:
@@ -22,27 +24,28 @@ while True:
     else:
         in_game = True
     
-    user = User(username=f"{username_input}")
-    session.add(user)
-    session.commit()
 
-    
     if in_game is True:
         print (f"\nHello {username_input}!")
     ## instructions go here
         sleep(1)
-        print ("\nTake a seat and let me explain how the sorting hat works.\n")
+        print ("\nTake a seat and let me explain how the sorting hat works.")
         sleep(1)
         print ("\nI will ask you a series of questions to get an idea of your character.")
+        sleep(.5)
+        print ("\nYou will answer either a, b, or c")
         sleep(1)
         print ("\nOnce you have reached the end of the questions,")
         sleep(.5)
         print ("\nI will reveal what house you belong to.")
         sleep(1.5)
 
-    
         take_quiz = input ("\nAre you ready to begin? (yes/no) ").lower()
         if take_quiz == "yes":
+            # user = User(username=f"{username_input}")
+            # session.add(user)
+            # session.commit()
+
             print ("\nGreat!! Here is your first question\n")
             
             answers = []
@@ -69,17 +72,19 @@ while True:
             
             most_repeated = generate_results(answers)
             house_info = generate_house()
+            print (("\nLet me think..."))
+            sleep(5)
             for house in house_info:
                 if most_repeated == house.id:
                     print(f"CONGRATS! You are a {house.housename}")
-
-                    result = Result(user_id=f"{user.id}", house_id=f"{house.id}")
-                    session.add(result)
-                    session.commit()
+                    for logo in logos:
+                        if house.housename == logo.name:
+                            print(logo.art)
+                    # result = Result(user_id=f"{user.id}", house_id=f"{house.id}")
+                    # session.add(result)
+                    # session.commit()
                     sys.exit(-1)
-                        
 
-        
                     
         elif take_quiz == "no":
             print ("\nPlease turn in your wand and leave Hogwarts immediately. Goodbye!")
