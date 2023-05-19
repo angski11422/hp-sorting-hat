a699c1721e31dc0204035ffefc04526207cb253d
Angela Palaszewski <delarosa.ang@gmail.com>
2023-05-19 11:30:06 -0600

    fixed loop error, commit user after start of quiz

--- lib/main.py
+++ lib/main.py
@@ -23,8 +23,6 @@ while True:
         print(Fore.RED+Style.BRIGHT+"Error: Please enter a valid name.")
     else:
         in_game = True
-        commit_name(username_input)
-        
     
         #instructions
         if in_game is True:
@@ -53,68 +51,69 @@ while True:
             print (Fore.MAGENTA+"\nI will reveal what house you belong to.")
             sleep(1.5)
 
-            in_quiz = False
-            take_quiz = input (Fore.GREEN+Style.BRIGHT+"\nAre you ready to begin? (yes/no) ").lower()
-            if take_quiz == "yes":
-                in_quiz = True
-                print (Fore.CYAN+Style.BRIGHT+wizard)
-                hat_begin()
-                sleep(.5)
-                print (Fore.GREEN+Style.BRIGHT+"\nHere is your first question\n")
-                
-                answers = []
-                random_questions = generate_questions()
-                index = 0
-            #creating random questions
-                while index < len(random_questions):
-                    question = random_questions[index]
-                    print (Style.BRIGHT+f"\n{question}")
-                    q_input = input(Fore.GREEN+Style.BRIGHT+"Your answer: \n").lower()
-                    if q_input == "a":
-                        answers.append(question.value_a)
-                        print(Style.RESET_ALL)
-                        index += 1
-                    elif q_input == "b":
-                        answers.append(question.value_b)
-                        print(Style.RESET_ALL)
-                        index += 1
-                    elif q_input == "c":
-                        answers.append(question.value_c)
-                        print(Style.RESET_ALL)
-                        index += 1
-                    elif q_input == "d":
-                        answers.append(question.value_d)
-                        print(Style.RESET_ALL)
-                        index += 1
-                    else:
-                        print (Fore.RED+Style.BRIGHT+"\nAnswer a, b, c, or d")
-                
-            #getting results based on answers
-                most_repeated = generate_results(answers)
-                house_info = generate_house()
-                print (Fore.CYAN+Style.BRIGHT+("\nLet me think..."))
-                print (Fore.CYAN+Style.BRIGHT+wand)
-                hat_sort()
-                sleep(3)
-                for house in house_info:
-                    if most_repeated == house.id:
-                        for logo in logos:
-                            if house.housename == logo.name:
-                                color = getattr(Fore, logo.color)
-                                print(color+Style.BRIGHT+f"CONGRATS! You are a {house.housename}")
-                                print(color+Style.BRIGHT+logo.art)
-                                userid = query_user(username_input)
-                                commit_result(userid, house.id)
-                                end()
-                                print (Fore.CYAN+"Now go join your house and have a great year!")
-                                sys.exit(-1)
-
-            elif take_quiz == "no":
-                print (Fore.RED+Back.WHITE+Style.BRIGHT+"\nPlease turn in your wand and leave Hogwarts immediately. Goodbye!")
-                sys.exit(-1)
-                
-            else:
-                print (Fore.RED+Style.BRIGHT+f"\nPlease enter yes or no.")
-                
-                   
+            while True:
+                take_quiz = input (Fore.GREEN+Style.BRIGHT+"\nAre you ready to begin? (yes/no) ").lower()
+                if take_quiz == "yes":
+                    commit_name(username_input)
+                    print (Fore.CYAN+Style.BRIGHT+wizard)
+                    hat_begin()
+                    sleep(.5)
+                    print (Fore.GREEN+Style.BRIGHT+"\nHere is your first question\n")
                     
+                    answers = []
+                    random_questions = generate_questions()
+                    index = 0
+                #creating random questions
+                    while index < len(random_questions):
+                        question = random_questions[index]
+                        print (Style.BRIGHT+f"\n{question}")
+                        q_input = input(Fore.GREEN+Style.BRIGHT+"Your answer: \n").lower()
+                        if q_input == "a":
+                            answers.append(question.value_a)
+                            print(Style.RESET_ALL)
+                            index += 1
+                        elif q_input == "b":
+                            answers.append(question.value_b)
+                            print(Style.RESET_ALL)
+                            index += 1
+                        elif q_input == "c":
+                            answers.append(question.value_c)
+                            print(Style.RESET_ALL)
+                            index += 1
+                        elif q_input == "d":
+                            answers.append(question.value_d)
+                            print(Style.RESET_ALL)
+                            index += 1
+                        else:
+                            print (Fore.RED+Style.BRIGHT+"\nAnswer a, b, c, or d")
+                    
+                #getting results based on answers
+                    most_repeated = generate_results(answers)
+                    house_info = generate_house()
+                    print (Fore.CYAN+Style.BRIGHT+("\nLet me think..."))
+                    print (Fore.CYAN+Style.BRIGHT+wand)
+                    hat_sort()
+                    sleep(3)
+                    for house in house_info:
+                        if most_repeated == house.id:
+                            for logo in logos:
+                                if house.housename == logo.name:
+                                    color = getattr(Fore, logo.color)
+                                    print(color+Style.BRIGHT+f"CONGRATS! You are a {house.housename}")
+                                    print(color+Style.BRIGHT+logo.art)
+                                    userid = query_user(username_input)
+                                    commit_result(userid, house.id)
+                                    end()
+                                    print (Fore.CYAN+"Now go join your house and have a great year!")
+                                    sys.exit(-1)
+
+                elif take_quiz == "no":
+                    print (Fore.RED+Back.WHITE+Style.BRIGHT+"\nPlease turn in your wand and leave Hogwarts immediately. Goodbye!")
+                    sys.exit(-1)
+                    
+                else:
+                    print (Fore.RED+Style.BRIGHT+f"\nPlease enter yes or no.")
+                    
+                    
+                    
+                        
